ef3b31c8ad2272221f53f6d6097f5c700d5695e1
swatcat87 <161406504+swatcat87@users.noreply.github.com>
2024-02-27 08:34:39 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+player.onChat("\"chicken\"", function () {
+    for (let index = 0; index < 4; index++) {
+        mobs.spawn(CHICKEN, pos(0, 0, 0))
+        mobs.spawn(CHICKEN, pos(0, 10, 0))
+    }
+})
