5c086b095c83432b132d8422fc675919f0f3eea2
swatcat87 <161406504+swatcat87@users.noreply.github.com>
2024-02-27 08:39:26 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+player.onChat("jump", function () {
+    for (let index = 0; index < 4; index++) {
+        mobs.spawn(CHICKEN, pos(0, 0, 0))
+        mobs.spawn(CHICKEN, pos(0, 10, 0))
+    }
+})
 player.onChat("\"chicken\"", function () {
     for (let index = 0; index < 4; index++) {
         mobs.spawn(CHICKEN, pos(0, 0, 0))
